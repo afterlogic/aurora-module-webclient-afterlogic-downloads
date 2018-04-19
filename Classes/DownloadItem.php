7bf8eca560f29d7db7e7bae6709c7052b9b7fa43
vasil <vasil.sokolov@gmail.com>
2018-04-19 14:16:08 +0300

    license annotations changes

--- Classes/DownloadItem.php
+++ Classes/DownloadItem.php
@@ -1,26 +1,15 @@
 <?php
 /**
- * @copyright Copyright (c) 2017, Afterlogic Corp.
- * @license AGPL-3.0
- *
- * This code is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License, version 3,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License, version 3,
- * along with this program. If not, see <http://www.gnu.org/licenses/>
- * 
- * @package Modules
+ * This code is licensed under AfterLogic Software License.
+ * For full statements of the license see LICENSE file.
  */
 
 namespace Aurora\Modules\AfterlogicDownloadsWebclient\Classes;
 
 /**
+ * @license https://afterlogic.com/products/common-licensing AfterLogic Software License
+ * @copyright Copyright (c) 2018, Afterlogic Corp.
+ *
  * @property int $IdUser
  *
  * @ignore
--- Manager.php
+++ Manager.php
@@ -1,27 +1,16 @@
 <?php
 /**
- * @copyright Copyright (c) 2017, Afterlogic Corp.
- * @license AGPL-3.0
- *
- * This code is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License, version 3,
- * as published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License, version 3,
- * along with this program. If not, see <http://www.gnu.org/licenses/>
- * 
- * @package Modules
+ * This code is licensed under AfterLogic Software License.
+ * For full statements of the license see LICENSE file.
  */
 
 namespace Aurora\Modules\AfterlogicDownloadsWebclient;
 
 /**
  * CApiContactsManager class summary
+ * 
+ * This code is licensed under AfterLogic Software License.
+ * For full statements of the license see LICENSE file.
  * 
  * @package ContactsMain
  */
