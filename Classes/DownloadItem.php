37274b84f0df0c3b4e6a5ff59848a664ddee439a
nadine-shi <nadine@afterlogic.com>
2019-04-26 14:13:05 +0300

    License and other texts improving

--- Classes/DownloadItem.php
+++ Classes/DownloadItem.php
@@ -1,20 +1,16 @@
 <?php
 /**
- * This code is licensed under Afterlogic Software License.
- * For full statements of the license see LICENSE file.
+ * This code is licensed under AGPLv3 license or Afterlogic Software License
+ * if commercial version of the product was purchased.
+ * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
  */
 
 namespace Aurora\Modules\AfterlogicDownloadsWebclient\Classes;
 
 /**
+ * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
  * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
  * @copyright Copyright (c) 2019, Afterlogic Corp.
- *
- * @property int $IdUser
- *
- * @ignore
- * @package Contactsmain
- * @subpackage Classes
  */
 class DownloadItem extends \Aurora\System\EAV\Entity
 {
--- Manager.php
+++ Manager.php
@@ -1,15 +1,16 @@
 <?php
 /**
- * This code is licensed under Afterlogic Software License.
- * For full statements of the license see LICENSE file.
+ * This code is licensed under AGPLv3 license or Afterlogic Software License
+ * if commercial version of the product was purchased.
+ * For full statements of the licenses see LICENSE-AFTERLOGIC and LICENSE-AGPL3 files.
  */
 
 namespace Aurora\Modules\AfterlogicDownloadsWebclient;
 
 /**
- * CApiContactsManager class summary
- * 
- * @package ContactsMain
+ * @license https://www.gnu.org/licenses/agpl-3.0.html AGPL-3.0
+ * @license https://afterlogic.com/products/common-licensing Afterlogic Software License
+ * @copyright Copyright (c) 2019, Afterlogic Corp.
  */
 class Manager extends \Aurora\System\Managers\AbstractManager
 {
